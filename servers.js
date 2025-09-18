3d0eff820d0f51c95f707c29108409992707761e
Y_Ramu <yarramsettiramu999@gmail.com>
2025-09-18 22:56:16 +0530

    updated to nodejs project

---
+++ servers.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Serve static files from public/
+app.use(express.static(path.join(_dirname, 'public')));
+
+//Fallback route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port, () => {
+    console.log('App running on http://localhost:${port}');
+});
