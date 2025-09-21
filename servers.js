bdb13aaf6c266d2c2be372912ba0bd4f68f6d701
Y_Ramu <yarramsettiramu999@gmail.com>
2025-09-21 14:45:41 +0530

    Chnaged Initial Code

--- servers.js
+++ servers.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-//Serve static files from public/
-app.use(express.static(path.join(_dirname, 'public')));
+// Serve static files from public/
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Fallback route
+// Fallback route
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(port, () => {
-    console.log('App running on http://localhost:${port}');
+  console.log(`App running on http://localhost:${port}`);
 });
